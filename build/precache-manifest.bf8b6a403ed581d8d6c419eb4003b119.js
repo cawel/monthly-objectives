cbaff8b03e2492cc0fb3a7708f97d8c6e5ff8f97
Martin Carel <cawel00@yahoo.com>
2020-05-07 17:52:58 -0400

    add build/ folder

---
+++ build/precache-manifest.bf8b6a403ed581d8d6c419eb4003b119.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8602b4246332e9f96db6a28b05dde76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "854a928fe5b334434b79",
+    "url": "/static/css/main.e3ea9be5.chunk.css"
+  },
+  {
+    "revision": "b1e5ce2efc7737981186",
+    "url": "/static/js/2.504eac95.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/static/js/2.504eac95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "854a928fe5b334434b79",
+    "url": "/static/js/main.5330fba4.chunk.js"
+  },
+  {
+    "revision": "69cea8f8b32882e709da",
+    "url": "/static/js/runtime-main.a941a003.js"
+  }
+]);
