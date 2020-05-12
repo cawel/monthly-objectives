dc0f486c450b0ee39052d2680ee2abd9b896ba5d
Martin Carel <cawel00@yahoo.com>
2020-05-12 18:10:10 -0400

    rename main app component

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { App } from "./App";
+import { ObjectivesApp } from "./ObjectivesApp";
 import NotFound from "./NotFound";
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={App} />
-        <Route exact path="/:year/:month" component={App} />
+        <Route exact path="/" component={ObjectivesApp} />
+        <Route exact path="/:year/:month" component={ObjectivesApp} />
         <Route exact component={NotFound} />
       </Switch>
     </BrowserRouter>
