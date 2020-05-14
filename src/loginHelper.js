c6652157eb7a53c94cc933c5a228d7478eb1fa71
Martin Carel <cawel00@yahoo.com>
2020-05-14 11:10:40 -0400

    add async function to know if a user is logged in

--- src/loginHelper.js
+++ src/loginHelper.js
@@ -1,3 +1,15 @@
 import { firebaseApp } from "./base";
 
+// this is a synchronous function
+// it will fail if firebase is not not initializing
+// see: https://firebase.google.com/docs/auth/web/manage-users
 export const isLoggedIn = () => firebaseApp.auth().currentUser !== null;
+
+export const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = firebaseApp.auth().onAuthStateChanged((user) => {
+      unsubscribe();
+      resolve(user);
+    }, reject);
+  });
+};
